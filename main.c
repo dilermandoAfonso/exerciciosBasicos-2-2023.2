8d8e01dbf7dfc6075495c1564bb1f8e7650424f0
Dilermando Afonso <125399148+dilermandoAfonso@users.noreply.github.com>
2023-08-31 14:02:24 -0300

    Add files via upload
    Maior ou Menor Número(Vetorizado).

--- main.c
+++ main.c
@@ -2,29 +2,33 @@
 #include <stdio.h>
 
 int main()
-{   
-    int num = 0;
-    int numMaior = 0;
-    int numMenor = 9999999;
+{
+    int numMaior, numMenor;
+    int vetor[4];
+    int i;
     
-    for(int i=0;i<4;i++) {
-        
+    for(i=0;i<4;i++) {
         do {
-            printf("Digite o número:");
-            scanf("%d",&num);
-            if(num<0) {
-                printf("O número deve ser positivo\n\n");
+            printf("Digite o número: ");
+            scanf("%d", &vetor[i]);
+            
+            if (vetor[i]<0){
+                printf("Precisa ser número positivo\n");
             }
-        } while(num<0);
+        } while(vetor[i]<0);
         
-        if (num > numMaior){
-            numMaior = num;
-        }
-        else if (num < numMenor) {
-            numMenor = num;
+        if(i==0){
+            numMaior = vetor[i];
+            numMenor = vetor[i];
         }
         
-        printf("Número Maior: %d", numMaior);
-        printf("Número Menor: %d", numMenor);
-    } 
+        if (vetor[i]>numMaior) {
+            numMaior = vetor[i];
+        } else if (vetor[i]<numMenor) {
+            numMenor = vetor[i];
+        }
+    }
+    
+    printf("Número menor: %d\n", numMenor);
+    printf("Número maior: %d", numMaior);
 }
